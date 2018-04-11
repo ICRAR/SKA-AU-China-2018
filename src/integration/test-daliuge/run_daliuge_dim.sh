f2676b913188d159d2a1a9ed9c1f650d9271cd81
KevinVinsen <kvn1961@gmail.com>
2018-04-11 12:36:05 +0800

    Getting DIM working

--- src/integration/test-daliuge/run_daliuge_dim.sh
+++ src/integration/test-daliuge/run_daliuge_dim.sh
@@ -4,4 +4,4 @@ source /home/kvinsen/virtualenv/shanghai/bin/activate
 
 export DLG_ROOT=/tmp/daliuge/`hostname`
 mkdir -p ${DLG_ROOT}
-dlg dim --daemon -vvv --port=8002 --log-dir /tmp/daliuge/dim.log --nodes 192.168.0.101,192.168.0.102,192.168.0.103,192.168.0.104
+dlg dim --daemon -vvv -H 202.127.29.97 --port=8002 --log-dir /tmp/daliuge/dim.log --nodes 192.168.0.101,192.168.0.102,192.168.0.103,192.168.0.104

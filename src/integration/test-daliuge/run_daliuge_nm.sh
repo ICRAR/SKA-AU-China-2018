27131156986a668a4473df5d99c2ebc9ef375d87
KevinVinsen <kvn1961@gmail.com>
2018-04-11 12:15:30 +0800

    Rodrigo fixed a bug in Daliuge

--- src/integration/test-daliuge/run_daliuge_nm.sh
+++ src/integration/test-daliuge/run_daliuge_nm.sh
@@ -2,5 +2,6 @@
 
 source /home/cwu/python27/bin/activate
 
-export DLG_ROOT=/tmp/`hostname`
+export DLG_ROOT=/tmp/daliuge/`hostname`
+mkdir -p ${DLG_ROOT}
 dlg nm --daemon -vvv -H 0.0.0.0 --log-dir /tmp/daliuge

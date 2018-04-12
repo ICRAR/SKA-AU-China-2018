d4528ad39b26ed72769e045b6335efc5e4955de2
KevinVinsen <kvn1961@gmail.com>
2018-04-12 13:07:01 +0800

    Wait for drop

--- src/integration/code/integration_common/wait_for_file_drop.py
+++ src/integration/code/integration_common/wait_for_file_drop.py
@@ -2,6 +2,8 @@
 Wait for a file to appear from
 """
 import logging
+from os.path import join
+
 from os import listdir
 from time import sleep
 
@@ -36,7 +38,7 @@ class WaitForFile(BarrierAppDROP):
         found = False
         while not found:
             sleep(5)
-            for file in listdir(self._directory_to_check):
+            for file in listdir(join(self._root_directory, self._directory_to_check)):
                 if file.startswith(self._starts_with):
                     found = True
                     break

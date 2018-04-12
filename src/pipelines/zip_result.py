ed9caa6336ebfde20d4ce026684bf463d8a0ca24
Shoulin Wei <weishoulin@astrolab.cn>
2018-04-12 13:58:16 +0800

    add zip

---
+++ src/pipelines/zip_result.py
@@ -0,0 +1,44 @@
+import os
+import argparse
+import commands
+import zipfile
+
+def parseResultDir(config_file) :
+    resultDir = None
+
+    with open(config_file, 'r') as configFile:
+    	lines = configFile.readlines()
+    	for line in lines:
+    		if line.index('Selavy.resultsFile') > -1 :
+    			resultDir = os.path.dirname(line.replace('Selavy.resultsFile = ', '').strip())
+
+    return resultDir
+
+def zipResult(result_dir):
+	zipFileName = "%s/%s.zip" % (result_dir, os.path.basename(result_dir))
+
+	zf = zipfile.ZipFile(zipFileName, "w", zipfile.zlib.DEFLATED)
+
+	for file in os.listdir():
+		zf.write(os.path.join(result_dir, file),file)
+
+	zf.close()
+
+	return zipFileName
+
+if __file__ == '__main__':
+    parser = argparse.ArgumentParser(description='Push to NGAS')
+    # parser.add_argument('--configfile', dest='config_file', help='each line is an image path',
+    #                     default=None, type=str)
+    parser.add_argument('--config_file', dest='config_file', help='path of config file',
+                        default=None, type=str) 
+	parser.add_argument('--tar_file', dest='tar_file', help='path of tar file',
+                        default=None, type=str) 
+    args = parser.parse_args()
+    resultDir = parseResultDir(args.config_file)
+    zip_path = zipResult(resultDir)
+
+    with open(args.tar_file) as fout:
+    	fout.write(zip_path)
+
+

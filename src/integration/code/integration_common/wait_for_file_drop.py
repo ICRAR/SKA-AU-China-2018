bdb168faae86e40dcb2ecb7fcae3a5ea3042fa69
KevinVinsen <kvn1961@gmail.com>
2018-04-12 12:56:07 +0800

    Wait for drop

--- src/integration/code/integration_common/wait_for_file_drop.py
+++ src/integration/code/integration_common/wait_for_file_drop.py
@@ -23,14 +23,12 @@ class WaitForFile(BarrierAppDROP):
         self._starts_with = self._getArg(kwargs, 'starts_with', None)
 
     def run(self):
-        file_list = list(reversed(sorted(listdir(self._root_directory))))
-        LOGGER.info('Type: {}'. format(type(file_list)))
-        for filename in file_list:
+        for filename in listdir(self._root_directory):
             LOGGER.info('Looking at {}'.format(filename))
             if filename.startswith('dlg_work_dir_'):
                 LOGGER.info('Found {}'.format(filename))
-                self._directory_to_check = filename
-                break
+                if filename > self._directory_to_check:
+                    self._directory_to_check = filename
 
         LOGGER.info('Looking in {}'.format(self._directory_to_check))
         found = False

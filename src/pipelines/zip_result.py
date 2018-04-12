6404649c370d73fb55710c6dc33adfb6bbdcf68d
Chen <chen.wu@uwa.edu.au>
2018-04-12 14:54:54 +0800

    fix zip

--- src/pipelines/zip_result.py
+++ src/pipelines/zip_result.py
@@ -11,17 +11,16 @@ def parseResultDir(config_file) :
         with open(configfile_path, 'r') as fin:
             lines = fin.readlines()
             for line in lines:
-        		if line.index('Selavy.resultsFile') > -1 :
+        		if line.find('Selavy.resultsFile') > -1 :
         			resultDir = os.path.dirname(line.replace('Selavy.resultsFile = ', '').strip())
 
     return resultDir
 
 def zipResult(result_dir):
 	zipFileName = "%s/%s.zip" % (result_dir, os.path.basename(result_dir))
-
+	filelist = os.listdir(result_dir)
 	zf = zipfile.ZipFile(zipFileName, "w", zipfile.zlib.DEFLATED)
 
-    	filelist = os.listdir(result_dir)
 
 	for file in filelist:
 		zf.write(os.path.join(result_dir, file))
@@ -30,7 +29,7 @@ def zipResult(result_dir):
 
 	return zipFileName
 
-if __name_ == '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Push to NGAS')
     # parser.add_argument('--configfile', dest='config_file', help='each line is an image path',
     #                     default=None, type=str)
